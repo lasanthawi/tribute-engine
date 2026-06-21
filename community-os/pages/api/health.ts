c937102cae2524b67e7c5298b22520a140f6d52f
lasanthawi <84735780+lasanthawi@users.noreply.github.com>
2026-06-21 23:06:39 +0530

    remove duplicate folder

--- community-os/pages/api/health.ts
+++
@@ -1,5 +0,0 @@
-import { NextApiRequest, NextApiResponse } from 'next'
-
-export default function handler(_req: NextApiRequest, res: NextApiResponse) {
-  res.status(200).json({ ok: true, service: 'community-os' })
-}
